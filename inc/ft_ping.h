f0f3efc4c5187d300a0af2229dca22508cbc733b
Minguk Gaang <minguk.gaang@gmail.com>
2023-04-29 12:15:32 +0200

    reorg

--- inc/ft_ping.h
+++ inc/ft_ping.h
@@ -13,6 +13,7 @@
 #include <stdbool.h>
 #include <assert.h>
 #include <errno.h>
+#include <netinet/ip_icmp.h>
 
 #define PACKET_SIZE 64
 #define ICMP_ECHO_REQUEST 8
@@ -37,65 +38,3 @@ typedef struct s_ping {
     char *host;
     struct addrinfo *addr;
 } t_ping;
-
-struct icmp {
-	u_char	icmp_type;		/* type of message, see below */
-	u_char	icmp_code;		/* type sub code */
-	u_short	icmp_cksum;		/* ones complement cksum of struct */
-	union {
-		u_char ih_pptr;			/* ICMP_PARAMPROB */
-		struct in_addr ih_gwaddr;	/* ICMP_REDIRECT */
-		struct ih_idseq {
-			uint16_t	icd_id;	/* network format */
-			uint16_t	icd_seq; /* network format */
-		} ih_idseq;
-		int ih_void;
-
-		/* ICMP_UNREACH_NEEDFRAG -- Path MTU Discovery (RFC1191) */
-		struct ih_pmtu {
-			uint16_t ipm_void;	/* network format */
-			uint16_t ipm_nextmtu;	/* network format */
-		} ih_pmtu;
-
-		struct ih_rtradv {
-			u_char irt_num_addrs;
-			u_char irt_wpa;
-			u_int16_t irt_lifetime;
-		} ih_rtradv;
-    } icmp_hun;
-#define	icmp_pptr	icmp_hun.ih_pptr
-#define	icmp_gwaddr	icmp_hun.ih_gwaddr
-#define	icmp_id		icmp_hun.ih_idseq.icd_id
-#define	icmp_seq	icmp_hun.ih_idseq.icd_seq
-#define	icmp_void	icmp_hun.ih_void
-#define	icmp_pmvoid	icmp_hun.ih_pmtu.ipm_void
-#define	icmp_nextmtu	icmp_hun.ih_pmtu.ipm_nextmtu
-#define	icmp_num_addrs	icmp_hun.ih_rtradv.irt_num_addrs
-#define	icmp_wpa	icmp_hun.ih_rtradv.irt_wpa
-#define	icmp_lifetime	icmp_hun.ih_rtradv.irt_lifetime
-// 	union {
-// 		struct id_ts {			/* ICMP Timestamp */
-// 			/*
-// 			 * The next 3 fields are in network format,
-// 			 * milliseconds since 00:00 GMT
-// 			 */
-// 			uint32_t its_otime;	/* Originate */
-// 			uint32_t its_rtime;	/* Receive */
-// 			uint32_t its_ttime;	/* Transmit */
-// 		} id_ts;
-// 		struct id_ip  {
-// 			struct ip idi_ip;
-// 			/* options and then 64 bits of data */
-// 		} id_ip;
-// 		struct icmp_ra_addr id_radv;
-// 		u_int32_t id_mask;
-// 		char	id_data[1];
-// 	} icmp_dun;
-// #define	icmp_otime	icmp_dun.id_ts.its_otime
-// #define	icmp_rtime	icmp_dun.id_ts.its_rtime
-// #define	icmp_ttime	icmp_dun.id_ts.its_ttime
-// #define	icmp_ip		icmp_dun.id_ip.idi_ip
-// #define	icmp_radv	icmp_dun.id_radv
-// #define	icmp_mask	icmp_dun.id_mask
-// #define	icmp_data	icmp_dun.id_data
-};
--- src/ft_ping.c
+++ src/ft_ping.c
@@ -2,6 +2,8 @@
 #include "libft.h"
 
 t_ping *g_ping_info;
+int g_sockfd;
+struct icmp g_packet;
 
 void exit_error(char *msg) {
     perror(msg);
@@ -24,8 +26,8 @@ int get_options(char *flag_str) {
     return (res);
 }
 
-t_ping* parse(int ac, char **av) {
-    t_ping* res = ft_calloc(1, sizeof(t_ping));
+void parse(int ac, char **av) {
+    g_ping_info = ft_calloc(1, sizeof(t_ping));
 
     for (int i = 1; i < ac; i++) {
         if (av[i][0] == '-') {
@@ -33,16 +35,15 @@ t_ping* parse(int ac, char **av) {
                 fprintf(stderr, "Error: Invalid flag\n");
                 exit(EXIT_FAILURE);
             }
-            res->flags |= get_options(av[i] + 1);
+            g_ping_info->flags |= get_options(av[i] + 1);
         } else {
-            if (res->host) {
+            if (g_ping_info->host) {
                 fprintf(stderr, "Error: Invalid flag\n");
                 exit(EXIT_FAILURE);
             }
-            res->host = av[i];
+            g_ping_info->host = av[i];
         }
     }
-    return (res);
 }
 
 void get_address() {
@@ -59,36 +60,6 @@ void get_address() {
         fprintf(stderr, "Not a valid IPv4 address\n");
         exit(EXIT_FAILURE);
     }
-}
-
-unsigned short checksum(void *b, int len) {
-    unsigned short *buf = b;
-    unsigned int sum = 0;
-    unsigned short result;
-
-    for (sum = 0; len > 1; len -= 2) {
-        sum += *buf++;
-    }
-    if (len == 1) {
-        sum += *(unsigned char*)buf;
-    }
-    sum = (sum >> 16) + (sum & 0xFFFF);
-    sum += (sum >> 16);
-    result = ~sum;
-    return result;
-}
-
-
-void terminate() {
-    freeaddrinfo(g_ping_info->addr);
-    free(g_ping_info);
-}
-
-int main(int ac, char **av) {
-    if (ac < 2)
-        exit(0);
-    g_ping_info = parse(ac, av);    
-    get_address();
 
     struct sockaddr_in *ipv4 = (struct sockaddr_in *) g_ping_info->addr->ai_addr;
 
@@ -97,7 +68,14 @@ int main(int ac, char **av) {
     printf("IP address: %s\n", ipstr);
     inet_pton(AF_INET, ipstr, &ipv4->sin_addr);
     printf("IP address: %s\n", ipstr);
+}
 
+void terminate() {
+    freeaddrinfo(g_ping_info->addr);
+    free(g_ping_info);
+}
+
+void init_socket() {
     // Ref: https://courses.cs.vt.edu/cs4254/fall04/slides/raw_1.pdf
     // Ref2: https://www.techtarget.com/searchnetworking/definition/time-to-live#:~:text=Time%2Dto%2Dlive%20(TTL)%20is%20a%20value%20for,lifetime%2C%20depends%20on%20the%20context.
     int sockfd = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
@@ -114,23 +92,30 @@ int main(int ac, char **av) {
         exit(EXIT_FAILURE);
     }
     printf("Socket options set.\n");
+}
 
-    // char buffer[576];
-    struct icmp *icmp_packet = {0};
+void create_packet() {
+    // Ref: https://courses.cs.vt.edu/cs4254/fall04/slides/raw_1.pdf
+    g_packet.icmp_type = ICMP_ECHO;
+    g_packet.icmp_code = 0;
+    g_packet.icmp_id = getpid();
+    g_packet.icmp_seq = 0;
+    g_packet.icmp_cksum = 0;
+}
 
-    // memset(buffer, 0, sizeof(buffer));
-    // icmp_packet = (struct icmp *)buffer;
-    icmp_packet->icmp_type = ICMP_ECHO;
-    icmp_packet->icmp_code = 0;
-    icmp_packet->icmp_id = getpid();
-    icmp_packet->icmp_seq = 0;
-    icmp_packet->icmp_cksum = 0;
-    icmp_packet->icmp_cksum = checksum(icmp_packet, sizeof(struct icmp));
+int main(int ac, char **av) {
+    if (ac < 2)
+        exit(0);
+    parse(ac, av);
+    get_address();
+    init_socket();
+    create_packet();    
 
-    printf("coucou\n");
+    
+
     while (1) {
         // send ping to the address
-        int ret = sendto(sockfd, icmp_packet, sizeof(struct icmp), 0, (struct sockaddr *)ipv4, sizeof(struct sockaddr_in)); 
+        int ret = sendto(g_sockfd, &packet, sizeof(struct icmp), 0, (struct sockaddr *)ipv4, sizeof(struct sockaddr_in)); 
         if (ret < 0)
             exit_error("ft_ping: sendto");
         usleep(1000000);
